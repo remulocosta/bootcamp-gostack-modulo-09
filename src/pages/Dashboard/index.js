df72310c14352174b6e2ab0b3c797d88d3ecb86c
Remulo Costa <remulo.costa@gmail.com>
2019-12-19 15:45:05 -0300

    requições autenticadas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
+import api from '~/services/api';
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  console.tron.log('Dashboard');
+  api.get('appointments');
   return <h1>Dashboard</h1>;
 }
